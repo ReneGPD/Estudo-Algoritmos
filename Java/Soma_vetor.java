5411f11741dabf752486fd3b3876ea4ffee271b3
René Portela <134896988+ReneGPD@users.noreply.github.com>
2026-07-23 15:52:47 +0000

    Add vetor exercice

---
+++ Java/Soma_vetor.java
@@ -0,0 +1,38 @@
+/*
+Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida:
+- Imprimir todos os elementos do vetor
+- Mostrar na tela a soma e a média dos elementos do veto
+ */
+
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+       Scanner sc = new Scanner(System.in);
+
+       int n = sc.nextInt();
+       double[] number = new double[n];
+       double values = 0;
+
+
+       for (int i = 0; i < n; i++) {
+           System.out.println("Digite um numero: ");
+           number[i] = sc.nextDouble();
+
+       }
+
+       values += number[0];
+       for (int i = 1; i < n; i++) {
+           values += number[i];
+       }
+
+       for (int i = 0; i < n; i++) {
+           System.out.println("Numeros digitados: " + number[i]);
+       }
+
+       double average = values / n;
+
+
+    System.out.println("Valores somados: " + values);
+    System.out.println("Media de valores: " + average);
+    }
+
