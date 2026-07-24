8e2285b8443357fcdd27074709a4a5114a37334a
René Portela <134896988+ReneGPD@users.noreply.github.com>
2026-07-24 17:50:56 -0300

    Add vectors exercices

---
+++ Java/Vetores_Pensionato/Main.java
@@ -0,0 +1,73 @@
+/*
+A dona de um pensionato possui dez quartos para alugar para estudantes,
+sendo esses quartos identificados pelos números 0 a 9.
+Fazer um programa que inicie com todos os dez quartos
+vazios, e depois leia uma quantidade N
+representando o número de estudantes
+que vão alugar quartos (N pode ser de 1 a 10). Em seguida,
+registre o aluguel dos N estudantes.
+P ara cada registro de aluguel, informar o nome e email do estudante,
+bem como qual dos quartos ele escolheu (de 0 a 9).
+Suponha que seja escolhido um quarto vago.
+Ao final, seu programa deve imprimir um relatório
+de todas ocupações do pensionato, por ordem de quarto, conforme exemplo.
+
+How many rooms will be rented? 3
+Rent #1:
+Name: Maria Green
+Email: maria@gmail.com
+Room: 5
+
+Rent #2:
+Name: Marco Antonio
+Email: marco@gmail.com
+Room: 1
+
+Rent #3:
+Name: Alex Brown
+Email: alex@gmail.com
+Room: 8
+
+Busy rooms:
+1: Marco Antonio, marco@gmail.com
+5: Maria Green, maria@gmail.com
+8: Alex Brown, alex@gmail.com
+*/
+
+void main() {
+
+    Scanner sc = new Scanner(System.in);
+
+    Rent[] vect = new Rent[10];
+
+    System.out.println("How many rooms will be retend? ");
+    int n = sc.nextInt();
+//Percorre e atribui os dados ao vetor
+    for (int i = 1; i < n; i++) {
+        System.out.println();
+        System.out.println("Rent #" + i  + ": ");
+        System.out.println("Name: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.println("Email: ");
+        String email = sc.next();
+        System.out.println("Room: ");
+        int roomNumber = sc.nextInt();
+
+        vect[roomNumber] = new Rent(name, email);
+        System.out.println(vect[roomNumber]);
+
+    }
+//Percorre os dados do vetor ja preenchidos
+    System.out.println("Busy Rooms: ");
+    for (int i = 0; i < 10; i++) {
+        if (vect[i] != null) {
+            System.out.println(i + (": ") + vect[i]);
+        }
+
+    }
+        sc.close();
+    }
+
+
+
